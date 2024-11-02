c4f13a0f754b9ce2515502a29299f6d69dc522d2
de12161 <162108835+de12161@users.noreply.github.com>
2024-11-02 19:38:11 +0300

    Implement main functionality

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,47 @@
 fun main() {
-    println("Hello World!")
+    val salesman = Salesman()
+    val worldMap = WorldMap(
+        "Бийск",
+        "Барнаул",
+        "Санкт-Петербург",
+        "Москва",
+        "Пермь",
+        "Воронеж",
+        "Выборг",
+        "Галич",
+        "Волгоград",
+        "Владимир",
+        "Владивосток",
+        "Гай",
+        "Глазов",
+        "Киров",
+        "Кострома"
+    )
+
+    while (true) {
+        println("create - составить поезд")
+        println("exit - выход")
+        print("Выберите действие: ")
+        val input = readln()
+
+        if (input == "exit") break
+
+        if (input == "create") {
+            val destination = Train.Destination(worldMap.lookForCities(2))
+            println("Создано направление $destination")
+
+            salesman.sellTickets()
+            val ticketCount = salesman.lastSale
+            println("Продано $ticketCount билета(ов)")
+
+            val train = Train(destination, ticketCount)
+            println("Сформирован поезд")
+
+            train.depart()
+
+            continue
+        }
+
+        println("Команда не распознана")
+    }
 }
